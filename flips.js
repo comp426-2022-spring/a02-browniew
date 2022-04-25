38a23e995c663f117168221e62f4e0b7168935b5
Winfield Warren <winfieldwarren@unc.edu>
2022-04-25 12:13:59 -0400

    idk whats wrong

--- flips.js
+++ flips.js
@@ -5,11 +5,7 @@ const args = minimist(process.argv.slice(2))
 args['number']
 
 const number = args.number || 1;
-if(number == 1) {
-    console.log(coinFlip());
-}
-else {
-    const flips =  coinFlips(number);
-    console.log(flips);
-    console.log(countFlips(flips));
-}
+
+const flips =  coinFlips(number);
+console.log(flips);
+console.log(countFlips(flips));
